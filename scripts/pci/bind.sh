69814ba272a53fa4741a2c87a5f9f02b79312bf4
ninevlc <9vlc@proton.me>
2025-09-19 15:28:45 +0300

    pci scripts cleanup

--- scripts/pci/bind.sh
+++ scripts/pci/bind.sh
@@ -1,38 +1,22 @@
 #!/bin/sh
-set -euo pipefail
+# vim: noet:sw=2:ts=2:
+set -eu
 
 #
 # the amazing ppt script
 # prerequisites: the generic bhyve pci passthrough ones, aka:
-#   iommu enabled in uefi
-#   hw.vmm.amdvi.enable=1 set in loader settings
+#   iommu w/ virtualization enabled in uefi
+#   hw.vmm.amdvi.enable=1 set in loader settings (if on an amd machine)
 #   cpu that supports all virtualization stuff
 #
-# note: this is very buggy for usb controllers, please set them in loader settings
 
-# arc: 45/0/0 46/0/0
-# radeon: 6/0/0 6/0/1
-pptdevs="2/0/0 6/0/0 6/0/1 48/0/4"
-
-################
-
-err=0
-
-#
-# get the script directory
-#
-if script_dir="$(command -v "$0")"; then
-  script_dir="$(dirname "$(realpath "$script_dir")")"
+if [ "${pptdevs:-}" ]; then
+	pptdevs="$pptdevs"
 else
-  script_dir="$(dirname "$(realpath "$0")")"
+	pptdevs="${*:-}"
 fi
 
-#
-# load in a bunch of functions
-#
-. "$script_dir"/helpers.in
-
-################
+err=0
 
 #
 # are we root?
@@ -40,12 +24,36 @@ fi
 [ "$USER" = root ] || _l e "must be root to run this"
 
 #
+# get the script directory
+#
+if script_dir="$(command -v "$0")"; then
+	script_dir="$(dirname "$(realpath "$script_dir")")"
+else
+	script_dir="$(dirname "$(realpath "$0")")"
+fi
+
+#
+# load in a bunch of functions
+#
+. "$script_dir"/helpers.in
+
+#
+# did the script even get devices provided? 
+#
+if [ -z "${pptdevs:-}" ]; then
+	>&2 printf 'usage: %s\n' "$0"
+	>&2 echo "this script takes a device list from a pptdevs env variable"
+	>&2 echo "or passed arguments and binds them to the ppt driver"
+	exit 1
+fi
+
+#
 # check if we have bhyve vms running
 #
 if [ -d /dev/vmm ]; then
-  _l e "cannot run while bhyve VMs are active" \
-    "please stop the following VMs:" \
-    "    $(ls /dev/vmm|tr '\n' ' ')"
+	_l e "cannot run while bhyve VMs are active" \
+	"please stop the following VMs:" \
+	"    $(ls /dev/vmm|tr '\n' ' ')"
 fi
 
 #
@@ -53,19 +61,19 @@ fi
 #
 devices=""
 for device_ppt in $pptdevs; do
-  if ! _d_is_ppt_syntax "$device_ppt"; then
-    _l x "error: malformed ppt device: $device_ppt" 1>&2
-  else
-    device_pci="$(_d_conv_syntax_pci "$device_ppt")"
-    if ! driver="$(_d_get_driver "$device_pci")"; then
-     _l x "error: device does not exist: $device_pci"
-    fi
-  fi
-  if [ "$driver" = ppt ]; then
-    _l i "device $device_pci already bound to ppt, skipping"
-    continue
-  fi
-  devices="$device_pci $devices"
+	if ! _d_is_ppt_syntax "$device_ppt"; then
+		_l x "error: malformed ppt device: $device_ppt"
+	else
+		device_pci="$(_d_conv_syntax_pci "$device_ppt")"
+		if ! driver="$(_d_get_driver "$device_pci")"; then
+			_l x "error: device does not exist: $device_pci"
+		fi
+	fi
+	if [ "$driver" = ppt ]; then
+		_l i "device $device_pci already bound to ppt, skipping"
+		continue
+	fi
+	devices="$device_pci $devices"
 done
 
 [ "$err" -ne 0 ] && exit "$err"
@@ -74,7 +82,7 @@ done
 # and then we detach devices from their driver
 #
 for device in $devices; do
-  _d_detach "$device"
+	_d_detach "$device"
 done
 
 #
@@ -82,27 +90,27 @@ done
 #
 vmm_prev_loaded=1
 if ! kldstat -qn vmm; then
-  _l i "loading vmm.ko"
-  vmm_prev_loaded=0
-  kenv hw.vmm.amdvi.enable=1 2>/dev/null
-  kenv pptdevs="$pptdevs" 2>/dev/null
-  kldload vmm
+	_l i "loading vmm.ko"
+	vmm_prev_loaded=0
+	kenv hw.vmm.amdvi.enable=1 2>/dev/null
+	kenv pptdevs="$pptdevs" 2>/dev/null
+	kldload vmm
 fi
 
 #
 # manually attach ppt to whatever devices vmm ignored
 #
 for device in $devices; do
-  driver="$(_d_get_driver "$device")"
-  if [ "$driver" != ppt ]; then
-    if [ "$vmm_prev_loaded" -eq 0 ]; then
-      _l w "manually attaching ppt for device $device"
-      _l w "if bhyve ppt does not work, add the device to loader's pptdevs!"
-    fi
-    if ! devctl set driver -f "$device" ppt; then
-      _l x "attaching ppt for device $device failed"
-    fi
-  fi
+	driver="$(_d_get_driver "$device")"
+	if [ "$driver" != ppt ]; then
+		if [ "$vmm_prev_loaded" -eq 0 ]; then
+			_l w "manually attaching ppt for device $device"
+			_l w "if bhyve ppt does not work, add the device to loader's pptdevs!"
+		fi
+		if ! devctl set driver -f "$device" ppt; then
+			_l x "attaching ppt for device $device failed"
+		fi
+	fi
 done
 
 [ "$err" -ne 0 ] && exit "$err"
--- scripts/pci/probe.sh
+++ scripts/pci/probe.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+# vim: noet:sw=2:ts=2:
 # by 9vlc
-set -euo pipefail
+set -eu
 
-devices="6/0/0 6/0/1"
+devices="${pptdevs:-0/0/0}"
 
 #
 # get the script directory
 #
 if script_dir="$(command -v "$0")"; then
-  script_dir="$(dirname "$(realpath "$script_dir")")"
+	script_dir="$(dirname "$(realpath "$script_dir")")"
 else
-  script_dir="$(dirname "$(realpath "$0")")"
+	script_dir="$(dirname "$(realpath "$0")")"
 fi
 
 #
@@ -26,30 +27,30 @@ fi
 [ "$USER" = root ] || _l e "must be root to run this"
 
 if [ -z "${1:-}" ] || [ -z "${2:-}" ]; then
-  1>&2 echo "usage: probe.sh save/load dir"
-  exit 1
+	>&2 echo "usage: probe.sh save/load dir"
+	exit 1
 fi
 
 state_dir="${2:-}"
 [ -d "$state_dir" ] || mkdir -p "$state_dir" 
 
 case "${1:-}" in
-  s*)
-    for dev in $devices; do
-      var_file="$state_dir/$(printf '%s' "$dev" | tr / _)"
+	s*)
+		for dev in $devices; do
+			var_file="$state_dir/$(printf '%s' "$dev" | tr / _)"
 
-      _d_save_conf "$(_d_conv_syntax_pci "$dev")" > "$var_file" &
-    done ; wait
-  ;;
-  l*)
-    [ -d "$state_dir" ] || _l e "no such state directory: $state_dir"
-    for dev in $devices; do
-      var_file="$state_dir/$(printf '%s' "$dev" | tr / _)"
-      [ -f "$var_file" ] || _l w "missing pci var file: $var_file"
+			_d_save_conf "$(_d_conv_syntax_pci "$dev")" > "$var_file"
+		done
+	;;
+	l*)
+		[ -d "$state_dir" ] || _l e "no such state directory: $state_dir"
+		for dev in $devices; do
+			var_file="$state_dir/$(printf '%s' "$dev" | tr / _)"
+			[ -f "$var_file" ] || _l w "missing pci var file: $var_file"
 
-      _l i "loading variables for $dev"
-      _d_load_conf "$(_d_conv_syntax_pci "$dev")" < "$var_file" &
-    done ; wait
-  ;;
-  *) echo "?" ; exit 1 ;;
+			_l i "loading variables for $dev"
+			_d_load_conf "$(_d_conv_syntax_pci "$dev")" < "$var_file"
+		done
+	;;
+	*) echo "?" ; exit 1 ;;
 esac
